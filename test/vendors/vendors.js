dd7196d648ade18527a7401f9f585f7cd46243c3
jc00ke <jesse@jc00ke.com>
2010-10-22 15:02:58 -0700

    Adding basic vendor test

--- test/vendors/vendors.js
+++ test/vendors/vendors.js
@@ -1,9 +1,18 @@
 
 $(document).ready(function() {
 
-    test("basic", function() {
-        expect(1);
-        ok(true, "true yo");
+    module("Vendor");
+
+    test("properties", function() {
+        expect(4);
+        var vendor = new Vendor({
+            name: "Bob's HVAC",
+            certs: ["hvac", "green"]
+        });
+        ok(vendor, "created");
+        equals("Bob's HVAC", vendor.get('name'), "name works");
+        ok(_.include(vendor.get('certs'), 'hvac'), "hvac cert is included");
+        ok(!_.include(vendor.get('certs'), 'red'), "red cert is not included");
     });
 
 });
