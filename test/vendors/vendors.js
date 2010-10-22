d578d6c38deaecd6872f0c6c227c2892fe63a09a
jc00ke <jesse@jc00ke.com>
2010-10-22 15:51:32 -0700

    Adding collection test using html in page

--- test/vendors/vendors.js
+++ test/vendors/vendors.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
     module("Vendor Collection sanity check");
 
-    test("collection", function() {
+    test("basic collection", function() {
         expect(1);
         var list = new VendorList;
         list.comparator = function(vendor) {
@@ -27,6 +27,25 @@ $(document).ready(function() {
         list.add(new Vendor({name: "Cooke's", certs: ["auction", "real_estate"]}));
         list.add(new Vendor({name: "Hoffman's", certs: ["perl", "real_estate"]}));
         equals(2, list.length, "length");
+    });
+
+    test("collection from page", function() {
+        var list = new VendorList;
+        list.comparator = function(vendor) {
+            return vendor.get('name');
+        }
+        $("#vendors li").each(function() {
+            var v = $(this);
+            list.add(
+                new Vendor({
+                    name:   $('.name', v).text(),
+                    certs:  _.map($('.solutions', v), function(solution) {
+                            return $(solution).html().replace(/\s|&\;\-/,'_').split(',');
+                    })
+                })
+            );
+        });
+        equals(78, list.length);
     });
 
 });
